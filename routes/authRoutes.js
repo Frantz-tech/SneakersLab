4c58474091516d83087ec5c30c2494ab6cee9864
Orsinet <frantz.o@icloud.com>
2025-03-28 09:34:34 +0100

    docs(authroutes.js): add protected route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { login, register } from "../controllers/authController.js";
-// import { authMiddleware } from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 export const router = express.Router();
 
 // Route protégée
-// router.get("/protected", authMiddleware, (res) => {
-//   res.status(200).json({ message: "Access granted to protected route" });
-// });
+router.get("/protected", authMiddleware, (res) => {
+  res.status(200).json({ message: "Access granted to protected route" });
+});
 // Route d'inscription
 router.post("/register", register);
 
