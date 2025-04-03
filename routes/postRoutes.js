bbe1be13aefd2a93dd61825fb0136ed45ca0f00d
Orsinet <frantz.o@icloud.com>
2025-04-03 16:21:02 +0200

    feat(postRoutes): modification route post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -11,12 +11,12 @@ const router = Router();
 
 router.post("/api/post", createPostController);
 
-router.get("/api/get/post/all", getPostController);
+router.get("/api/post/", getPostController);
 
-router.get("/api/get/post/:id", getPostByIdController);
+router.get("/api/post/:id", getPostByIdController);
 
-router.put("/api/update/post/:id", updatePostController);
+router.put("/api/post/:id", updatePostController);
 
-router.delete("/api/delete/post/:id", deletePostController);
+router.delete("/api/post/:id", deletePostController);
 
 export default router;
